4fd96f1593ec73e032f0848901d37dbfbb6d2283
Mikhail Eberil <i8megabit@gmail.com>
2025-02-14 23:15:21 +0300

    1.1.3z +

--- tools/k8s-kind-setup/setup-gpu.sh
+++ tools/k8s-kind-setup/setup-gpu.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Добавление taint на ноду
-kubectl taint nodes kind-control-plane nvidia.com/gpu=present:NoSchedule --overwrite
-
 # Добавление меток для GPU на control-plane ноду
 kubectl label node kind-control-plane nvidia.com/gpu=present --overwrite
-kubectl label node kind-control-plane kubernetes.io/os=linux --overwrite
+
+# Добавление taint для GPU
+kubectl taint nodes kind-control-plane nvidia.com/gpu=present:NoSchedule --overwrite
 
 # Проверка настроек
 echo "Проверка меток и taint узла:"
 kubectl get node kind-control-plane --show-labels
 kubectl describe node kind-control-plane | grep Taints
+
