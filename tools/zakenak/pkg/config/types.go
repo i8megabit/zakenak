aa8a8d33051018b4904a35971c4f6e366ceacb55
Mikhail Eberil <i8megabit@gmail.com>
2025-02-16 19:23:23 +0300

    1.3.2e +++ go types fix

--- tools/zakenak/pkg/config/types.go
+++ tools/zakenak/pkg/config/types.go
@@ -11,17 +11,18 @@ import (
 
 // Config представляет основную конфигурацию приложения
 type Config struct {
-    Docker  DockerConfig  `json:"docker"`
-    Helm    HelmConfig    `json:"helm"`
-    Runtime RuntimeConfig `json:"runtime"`
+    Docker   DockerConfig   `json:"docker"`
+    Helm     HelmConfig     `json:"helm"`
+    Runtime  RuntimeConfig  `json:"runtime"`
+    Deploy   DeployConfig   `json:"deploy"`
 }
 
 // DockerConfig содержит настройки для Docker
 type DockerConfig struct {
-    Host      string            `json:"host"`
-    TLS       bool             `json:"tls"`
-    CertPath  string           `json:"certPath"`
-    EventType events.Type      `json:"eventType"`
+    Host      string       `json:"host"`
+    TLS       bool        `json:"tls"`
+    CertPath  string      `json:"certPath"`
+    EventType events.Type `json:"eventType"`
 }
 
 // HelmConfig содержит настройки для Helm
@@ -35,4 +36,14 @@ type RuntimeConfig struct {
     Debug     bool   `json:"debug"`
     LogLevel  string `json:"logLevel"`
     GPUEnable bool   `json:"gpuEnable"`
+}
+
+// DeployConfig содержит настройки развертывания
+type DeployConfig struct {
+    Namespace     string            `json:"namespace"`
+    ChartPath     string           `json:"chartPath"`
+    ReleaseName   string           `json:"releaseName"`
+    Values        map[string]string `json:"values"`
+    WaitTimeout   int              `json:"waitTimeout"`
+    AutoRollback  bool             `json:"autoRollback"`
 }
