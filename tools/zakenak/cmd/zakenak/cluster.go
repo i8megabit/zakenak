79b18ea7efb80e5ae6362158db647a0a20ae75f3
Mikhail Eberil <57707377+i8megabit@users.noreply.github.com>
2025-06-19 10:35:21 +0300

    Improve CI checks and fix unused code

--- tools/zakenak/cmd/zakenak/cluster.go
+++ tools/zakenak/cmd/zakenak/cluster.go
@@ -1,18 +1,18 @@
 /*
  * Copyright (c) 2023-2025 Mikhail Eberil (@eberil)
- * 
+ *
  * This file is part of Ƶakenak, a GitOps deployment tool.
- * 
+ *
  * Ƶakenak is free software: you can redistribute it and/or modify
  * it under the terms of the MIT License with Trademark Protection.
- * 
+ *
  * Ƶakenak is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * MIT License for more details.
- * 
+ *
  * The name "Ƶakenak" and associated branding are  * Copyright (c) 2023-2025 Mikhail Eberil (@eberil)
- * 
+ *
  * This file is part of Zakenak project.
  * https://github.com/i8megabit/zakenak
  *
@@ -28,67 +28,64 @@
 package main
 
 import (
-    "github.com/spf13/cobra"
-    "github.com/i8megabit/zakenak/pkg/kind"
+	"github.com/i8megabit/zakenak/pkg/kind"
+	"github.com/spf13/cobra"
 )
 
-//lint:ignore U1000 CLI feature under development
 func newClusterCmd() *cobra.Command {
-    cmd := &cobra.Command{
-        Use:   "cluster",
-        Short: "Управление Kind кластером",
-    }
+	cmd := &cobra.Command{
+		Use:   "cluster",
+		Short: "Управление Kind кластером",
+	}
 
-    cmd.AddCommand(
-        newClusterCreateCmd(),
-        newClusterDeleteCmd(),
-    )
+	cmd.AddCommand(
+		newClusterCreateCmd(),
+		newClusterDeleteCmd(),
+	)
 
-    return cmd
+	return cmd
 }
 
-//lint:ignore U1000 CLI feature under development
 func newClusterCreateCmd() *cobra.Command {
-    var configPath string
-    var gpuEnabled bool
-    var clusterName string
+	var configPath string
+	var gpuEnabled bool
+	var clusterName string
 
-    cmd := &cobra.Command{
-        Use:   "create",
-        Short: "Создать новый Kind кластер",
-        RunE: func(cmd *cobra.Command, args []string) error {
-            cfg := kind.DefaultConfig()
-            cfg.GPUEnabled = gpuEnabled
+	cmd := &cobra.Command{
+		Use:   "create",
+		Short: "Создать новый Kind кластер",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cfg := kind.DefaultConfig()
+			cfg.GPUEnabled = gpuEnabled
 
-            if err := cfg.GenerateConfig(configPath); err != nil {
-                return err
-            }
+			if err := cfg.GenerateConfig(configPath); err != nil {
+				return err
+			}
 
-            manager := kind.NewManager(clusterName, configPath)
-            return manager.CreateCluster(cmd.Context())
-        },
-    }
+			manager := kind.NewManager(clusterName, configPath)
+			return manager.CreateCluster(cmd.Context())
+		},
+	}
 
-    cmd.Flags().StringVar(&configPath, "config", "kind-config.yaml", "путь к конфигурации Kind")
-    cmd.Flags().BoolVar(&gpuEnabled, "gpu", true, "включить поддержку GPU")
-    cmd.Flags().StringVar(&clusterName, "name", "zakenak", "имя кластера")
+	cmd.Flags().StringVar(&configPath, "config", "kind-config.yaml", "путь к конфигурации Kind")
+	cmd.Flags().BoolVar(&gpuEnabled, "gpu", true, "включить поддержку GPU")
+	cmd.Flags().StringVar(&clusterName, "name", "zakenak", "имя кластера")
 
-    return cmd
+	return cmd
 }
 
-//lint:ignore U1000 CLI feature under development
 func newClusterDeleteCmd() *cobra.Command {
-    var clusterName string
-    
-    cmd := &cobra.Command{
-        Use:   "delete",
-        Short: "Удалить Kind кластер",
-        RunE: func(cmd *cobra.Command, args []string) error {
-            manager := kind.NewManager(clusterName, "kind-config.yaml")
-            return manager.DeleteExistingCluster(cmd.Context())
-        },
-    }
+	var clusterName string
 
-    cmd.Flags().StringVar(&clusterName, "name", "zakenak", "имя кластера")
-    return cmd
+	cmd := &cobra.Command{
+		Use:   "delete",
+		Short: "Удалить Kind кластер",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			manager := kind.NewManager(clusterName, "kind-config.yaml")
+			return manager.DeleteExistingCluster(cmd.Context())
+		},
+	}
+
+	cmd.Flags().StringVar(&clusterName, "name", "zakenak", "имя кластера")
+	return cmd
 }
